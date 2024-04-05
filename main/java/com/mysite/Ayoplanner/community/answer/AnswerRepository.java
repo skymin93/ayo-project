c50a1d4056b2ca5e43296ea0cfb729ba4f399f22
Yunseo <158689708+yunseo531@users.noreply.github.com>
2024-04-06 08:16:29 +0900

    Delete main/java/com/mysite/Ayoplanner/community/answer directory

--- main/java/com/mysite/Ayoplanner/community/answer/AnswerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mysite.Ayoplanner.community.answer;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AnswerRepository extends JpaRepository<Answer, Integer>{
-	
-}
