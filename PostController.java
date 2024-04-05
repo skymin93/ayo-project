adade03d3a4e2b69e197d8472e682b8b932198d5
Yunseo <158689708+yunseo531@users.noreply.github.com>
2024-04-06 08:23:36 +0900

    Add files via upload

---
+++ PostController.java
@@ -0,0 +1,186 @@
+package com.mysite.Ayoplanner.community.post;
+
+import java.security.Principal;
+
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.mysite.Ayoplanner.community.answer.Answer;
+import com.mysite.Ayoplanner.community.answer.AnswerForm;
+import com.mysite.Ayoplanner.community.answer.AnswerService;
+import com.mysite.Ayoplanner.community.category.Category;
+import com.mysite.Ayoplanner.community.category.CategoryService;
+import com.mysite.Ayoplanner.user.SiteUser;
+import com.mysite.Ayoplanner.user.UserService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+public class PostController {
+
+	private final PostService postService;
+	private final UserService userService;
+	private final AnswerService answerService;
+	private final CategoryService categoryService;
+
+	// postlist 첫화면
+	@GetMapping("post/list")
+	public String questionList(Model model, @RequestParam(value = "page", defaultValue = "0") int page,
+			@RequestParam(value = "kw", defaultValue = "") String kw) {
+		Page<Post> paging = this.postService.getList(page, kw, "질문");
+		model.addAttribute("paging", paging);
+		model.addAttribute("kw", kw);
+		return "post_list";
+	}
+
+	// post 상세 페이지
+	@GetMapping(value = "post/detail/{id}")
+	public String detail(Model model, @PathVariable("id") Integer id, AnswerForm answerForm,
+			@RequestParam(value = "answerPage", defaultValue = "0") int answerPage) {
+		Post post = this.postService.getPost(id);
+		Page<Answer> answerPaging = this.answerService.getList(post, answerPage);
+		model.addAttribute("post", post);
+		model.addAttribute("answerPaging", answerPaging);
+		return "post_detail";
+	}
+
+	// transportation list
+	@GetMapping("post/list/transportation")
+	public String transportationList(Model model, @RequestParam(value = "page", defaultValue = "0") int page,
+			@RequestParam(value = "kw", defaultValue = "") String kw) {
+		Page<Post> paging = this.postService.getList(page, kw, "교통");
+		model.addAttribute("paging", paging);
+		model.addAttribute("kw", kw);
+		return "post_list_transportation";
+	}
+
+	// tripplan list
+	@GetMapping("post/list/tripplan")
+	public String tripplanList(Model model, @RequestParam(value = "page", defaultValue = "0") int page,
+			@RequestParam(value = "kw", defaultValue = "") String kw) {
+		Page<Post> paging = this.postService.getList(page, kw, "여행일정");
+		model.addAttribute("paging", paging);
+		model.addAttribute("kw", kw);
+		return "post_list_tripplan";
+	}
+
+	// city list
+	@GetMapping("post/list/city")
+	public String cityList(Model model, @RequestParam(value = "page", defaultValue = "0") int page,
+			@RequestParam(value = "kw", defaultValue = "") String kw) {
+		Page<Post> paging = this.postService.getList(page, kw, "도시별");
+		model.addAttribute("paging", paging);
+		model.addAttribute("kw", kw);
+		return "post_list_city";
+	}
+
+	// expenses list
+	@GetMapping("post/list/expenses")
+	public String expensesList(Model model, @RequestParam(value = "page", defaultValue = "0") int page,
+			@RequestParam(value = "kw", defaultValue = "") String kw) {
+		Page<Post> paging = this.postService.getList(page, kw, "기타비용");
+		model.addAttribute("paging", paging);
+		model.addAttribute("kw", kw);
+		return "post_list_expenses";
+	}
+
+	// info list
+	@GetMapping("post/list/info")
+	public String infoList(Model model, @RequestParam(value = "page", defaultValue = "0") int page,
+			@RequestParam(value = "kw", defaultValue = "") String kw) {
+		Page<Post> paging = this.postService.getList(page, kw, "기타정보");
+		model.addAttribute("paging", paging);
+		model.addAttribute("kw", kw);
+		return "post_list_info";
+	}
+
+	// hotel list
+	@GetMapping("post/list/hotel")
+	public String hotelList(Model model, @RequestParam(value = "page", defaultValue = "0") int page,
+			@RequestParam(value = "kw", defaultValue = "") String kw) {
+		Page<Post> paging = this.postService.getList(page, kw, "숙소");
+		model.addAttribute("paging", paging);
+		model.addAttribute("kw", kw);
+		return "post_list_hotel";
+	}
+
+	@PreAuthorize("isAuthenticated()")
+	@GetMapping("post/create")
+	public String postCreate(Model model, PostForm questionForm) {
+		model.addAttribute("categoryList", categoryService.getList());
+		return "post_form";
+	}
+
+	// 카테고리이름으로 카테고리엔티티조회, 조회한카테고리엔티티를 질문 엔티티에 넣는다
+	@PreAuthorize("isAuthenticated()")
+	@PostMapping("post/create")
+	public String createPost(Model model, @Valid PostForm postForm, BindingResult bindingResult, Principal principal) {
+		if (bindingResult.hasErrors()) {
+			model.addAttribute("categoryList", categoryService.getList());
+			return "post_form";
+		}
+		SiteUser siteUser = this.userService.getUser(principal.getName());
+		Category category = this.categoryService.getCategory(postForm.getCategory());
+		this.postService.create(postForm.getSubject(), postForm.getContent(), siteUser, category);
+		return "redirect:/post/list";
+	}
+
+	@PreAuthorize("isAuthenticated()")
+	@GetMapping("post/modify/{id}")
+	public String postModify(PostForm postForm, @PathVariable("id") Integer id, Principal principal) {
+		Post post = this.postService.getPost(id);
+		if (!post.getAuthor().getUsername().equals(principal.getName())) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다");
+		}
+		postForm.setSubject(post.getSubject());
+		postForm.setContent(post.getContent());
+		postForm.setCategory(post.getSubject());
+		return "post_form";
+	}
+
+	@PreAuthorize("isAuthenticated()")
+	@PostMapping("post/modify/{id}")
+	public String postModify(@Valid PostForm postForm, BindingResult bindingResult, Principal principal,
+			@PathVariable("id") Integer id) {
+		if (bindingResult.hasErrors()) {
+			return "post_form";
+		}
+		Post post = this.postService.getPost(id);
+		if (!post.getAuthor().getUsername().equals(principal.getName())) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정 권한이 없습니다");
+		}
+		this.postService.modify(post, postForm.getSubject(), postForm.getContent(), postForm.getCategory());
+		return String.format("redirect:/post/detail/%s", id);
+	}
+
+	@PreAuthorize("isAuthenticated()")
+	@GetMapping("post/delete/{id}")
+	public String postDelete(Principal principal, @PathVariable("id") Integer id) {
+		Post question = this.postService.getPost(id);
+		if (!question.getAuthor().getUsername().equals(principal.getName())) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "삭제 권한이 없습니다.");
+		}
+		this.postService.delete(question);
+		return "rediect:/";
+	}
+
+	@PreAuthorize("isAuthenticated()")
+	@GetMapping("post/vote/{id}")
+	public String postVote(Principal principal, @PathVariable("id") Integer id) {
+		Post post = this.postService.getPost(id);
+		SiteUser siteUser = this.userService.getUser(principal.getName());
+		this.postService.vote(post, siteUser);
+		return String.format("redirect:/post/detail/%s", id);
+	}
+}
---
+++ PostService.java
@@ -0,0 +1,108 @@
+package com.mysite.Ayoplanner.community.post;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Service;
+
+import com.mysite.Ayoplanner.community.answer.Answer;
+import com.mysite.Ayoplanner.community.category.Category;
+import com.mysite.Ayoplanner.community.category.CategoryService;
+import com.mysite.Ayoplanner.exception.DataNotFoundException;
+import com.mysite.Ayoplanner.exception.ErrorCode;
+import com.mysite.Ayoplanner.user.SiteUser;
+
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Join;
+import jakarta.persistence.criteria.JoinType;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class PostService {
+
+	private final PostRepository postRepository;
+	private final CategoryService categoryService;
+
+	private Specification<Post> search(String kw, String categoryName) {
+		return new Specification<>() {
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public Predicate toPredicate(Root<Post> q, CriteriaQuery<?> query, CriteriaBuilder cb) {
+				query.distinct(true); // 중복 제거
+				Join<Post, SiteUser> u1 = q.join("author", JoinType.LEFT);
+				Join<Post, Answer> a = q.join("answerList", JoinType.LEFT);
+				Join<Post, Category> c = q.join("category", JoinType.LEFT);
+				Join<Answer, SiteUser> u2 = a.join("author", JoinType.LEFT);
+				return cb.and(cb.or(cb.like(q.get("subject"), "%" + kw + "%"), // 제목
+						cb.like(q.get("content"), "%" + kw + "%"), // 내용
+						cb.like(u1.get("username"), "%" + kw + "%"), // 질문 작성자
+						cb.like(a.get("content"), "%" + kw + "%"), // 답변 내용
+						cb.like(u2.get("username"), "%" + kw + "%")), // 답변 작성자
+						// and
+						cb.like(c.get("name"), "%" + categoryName + "%")); // 카테고리 이름
+			}
+		};
+	}
+
+	public void create(String subject, String content, SiteUser user, Category category) {
+		Post p = new Post();
+		p.setSubject(subject);
+		p.setContent(content);
+		p.setCreateDate(LocalDateTime.now());
+		p.setAuthor(user);
+		p.setCategory(category);
+		this.postRepository.save(p);
+	}
+
+	public void modify(Post post, String subject, String content, String categoryName) {
+		post.setSubject(subject);
+		post.setContent(content);
+		Category category = categoryService.getCategory(categoryName);
+		post.setCategory(category);
+		post.setModifyDate(LocalDateTime.now());
+		this.postRepository.save(post);
+	}
+
+	public void delete(Post post) {
+		this.postRepository.delete(post);
+	}
+
+	public void vote(Post post, SiteUser siteUser) {
+		post.getVoter().add(siteUser);
+		this.postRepository.save(post);
+	}
+
+	public Page<Post> getList(int page, String kw, String categoryName) {
+		List<Sort.Order> sorts = new ArrayList<>();
+		sorts.add(Sort.Order.desc("createDate"));
+		Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
+		Specification<Post> spec = search(kw, categoryName);
+		return this.postRepository.findAll(spec, pageable);
+	}
+
+	public List<Post> getCurrentListByUser(String username, int num) {
+		Pageable pageable = PageRequest.of(0, num);
+		return postRepository.findCurrentQuestion(username, pageable);
+	}
+
+	public Post getPost(Integer id) {
+		Optional<Post> post = this.postRepository.findById(id);
+		if (post.isPresent()) {
+			return post.get();
+		} else {
+			throw new DataNotFoundException(ErrorCode.RESOURCE_NOT_FOUND);
+		}
+	}
+}
