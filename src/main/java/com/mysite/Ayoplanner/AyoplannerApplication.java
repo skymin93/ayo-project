64fcfee82d22365f1dd2d1ed6377b5824647555d
Yunseo <158689708+yunseo531@users.noreply.github.com>
2024-04-06 09:00:14 +0900

    Create AyoplannerApplication.java

---
+++ src/main/java/com/mysite/Ayoplanner/AyoplannerApplication.java
@@ -0,0 +1,13 @@
+package com.mysite.Ayoplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AyoplannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AyoplannerApplication.class, args);
+	}
+
+}
