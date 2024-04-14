e24400083013d2258d2db3ee05b50c85bd8f81b6
Yunseo <158689708+yunseo531@users.noreply.github.com>
2024-04-14 21:10:46 +0900

    Update AnswerService.java

--- src/main/java/com/mysite/Ayoplanner/community/answer/AnswerService.java
+++ src/main/java/com/mysite/Ayoplanner/community/answer/AnswerService.java
@@ -22,7 +22,7 @@ import lombok.RequiredArgsConstructor;
 @Service
 public class AnswerService {
 	private final AnswerRepository answerRepository;
-	
+
 	public Answer create(Post post, String content, SiteUser author) {
 		Answer answer = new Answer();
 		answer.setContent(content);
@@ -32,41 +32,50 @@ public class AnswerService {
 		this.answerRepository.save(answer);
 		return answer;
 	}
-	
+
 	public Answer getAnswer(Integer id) {
 		Optional<Answer> answer = this.answerRepository.findById(id);
 		if (answer.isPresent()) {
 			return answer.get();
-		}else {
-			 throw new DataNotFoundException(ErrorCode.RESOURCE_NOT_FOUND);
+		} else {
+			throw new DataNotFoundException(ErrorCode.RESOURCE_NOT_FOUND);
 		}
 	}
-	
+
 	public void modify(Answer answer, String content) {
 		answer.setContent(content);
 		answer.setModifyDate(LocalDateTime.now());
 		this.answerRepository.save(answer);
 	}
-	
+
 	public void delete(Answer answer) {
 		this.answerRepository.delete(answer);
 	}
-	
+
 	public void vote(Answer answer, SiteUser siteUser) {
 		answer.getVoter().add(siteUser);
 		this.answerRepository.save(answer);
 	}
-	
-	public Page<Answer> getList(Post post, int page) {
-        List<Sort.Order> sorts = new ArrayList<>();
-        sorts.add(Sort.Order.desc("voter"));
-        sorts.add(Sort.Order.asc("createDate"));
-        Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
-        return this.answerRepository.findAllByPost(post, pageable);
-    }
 
-    public List<Answer> getCurrentListByUser(String username, int num) {
-        Pageable pageable = PageRequest.of(0, num);
-        return answerRepository.findCurrentAnswer(username, pageable);
-    }
+	/* 마이페이지에서 보여 줄 답변의 개수 */
+	public Long getAnswerCount(SiteUser author) {
+		return answerRepository.countByAuthor(author);
+	}
+
+	public List<Answer> getAnswerTop5LatestByUser(SiteUser user) {
+		return answerRepository.findTop5ByAuthorOrderByCreateDateDesc(user);
+	}
+
+	public Page<Answer> getList(Post post, int page) {
+		List<Sort.Order> sorts = new ArrayList<>();
+		sorts.add(Sort.Order.desc("voter"));
+		sorts.add(Sort.Order.asc("createDate"));
+		Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
+		return this.answerRepository.findAllByPost(post, pageable);
+	}
+
+	public List<Answer> getCurrentListByUser(String username, int num) {
+		Pageable pageable = PageRequest.of(0, num);
+		return answerRepository.findCurrentAnswer(username, pageable);
+	}
 }
