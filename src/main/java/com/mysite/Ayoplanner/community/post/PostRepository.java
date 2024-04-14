f695d3252922cb202d5059006dc33746e1853d90
Yunseo <158689708+yunseo531@users.noreply.github.com>
2024-04-14 21:28:11 +0900

    Update PostRepository.java

--- src/main/java/com/mysite/Ayoplanner/community/post/PostRepository.java
+++ src/main/java/com/mysite/Ayoplanner/community/post/PostRepository.java
@@ -10,15 +10,17 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import com.mysite.Ayoplanner.user.SiteUser;
+
 @Repository
 public interface PostRepository extends JpaRepository<Post, Integer>{
-	Post findBySubject(String subject); // findBy+엔티티의 속성이름(컬럼이름)  <-- JPA가 알아서
+	Post findBySubject(String subject);
 	Post findBySubjectAndContent(String subject, String content);
 	List<Post> findBySubjectLike(String subject);
 	Page<Post> findAll(Pageable pageable);
 	Page<Post> findAll(Specification<Post> spec, Pageable pageable);
-	    
-	 @Query("select q "
+	 
+	@Query("select q "
 		        + "from Post q "
 		        + "join SiteUser u on q.author=u "
 		        + "where u.username = :username "
@@ -39,4 +41,44 @@ public interface PostRepository extends JpaRepository<Post, Integer>{
 		        + "   or a.content like %:kw% "
 		        + "   or u2.username like %:kw% ")
 		    Page<Post> findAllByKeyword(@Param("kw") String kw, Pageable pageable);
+		    
+		    
+		    //질문자가 작성한 질문글만
+		    @Query("select "
+		    		+ "distinct q "
+		    		+ "from Post q "
+		    		+ "left outer join SiteUser u1 on q.author=u1 "
+		    		+ "left outer join Answer a on a.post=q "
+		    		+ "left outer join SiteUser u2 on a.author=u2 "
+		    		+ "where "
+		    		+ "   (q.author.id = :authorId) "
+		    		+ "   and ( "
+		    		+ "       q.subject like %:kw% "
+		    		+ "       or q.content like %:kw% "
+		    		+ "       or a.content like %:kw% "
+		    		+ "       or u2.username like %:kw% "
+		    		+ "   )")
+			Page<Post> findAllByKeywordAndAuthorId(@Param("kw") String kw, @Param("authorId") Long authorId, Pageable pageable);
+			
+		    //질문자가 작성한 답변글만
+		    @Query("select "
+		    		+ "distinct q "
+		    		+ "from Post q "
+		    		+ "left outer join SiteUser u1 on q.author = u1 "
+		    		+ "left outer join q.answerList a "
+		    		+ "left outer join SiteUser u2 on a.author = u2 "
+		    		+ "where "
+		    		+ "   (u2.id = :authorId) "
+		    		+ "   and ( "
+		    		+ "       q.subject like %:kw% "
+		    		+ "       or q.content like %:kw% "
+		    		+ "       or u1.username like %:kw% "
+		    		+ "       or a.content like %:kw% "
+		    		+ "       or u2.username like %:kw% "
+		    		+ "   )")
+		    Page<Post> findAllByKeywordAndAnswer_AuthorId(@Param("kw") String kw, @Param("authorId") Long authorId, Pageable pageable);
+			
+		    Long countByAuthor(SiteUser author);
+			List<Post> findTop5ByAuthorOrderByCreateDateDesc(SiteUser author);
+			
 }
