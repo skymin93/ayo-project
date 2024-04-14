176edbfc379acf04fb19ac2d7e71cafb9af003e5
Yunseo <158689708+yunseo531@users.noreply.github.com>
2024-04-14 21:05:42 +0900

    Update CommonUtil.java

--- src/main/java/com/mysite/Ayoplanner/CommonUtil.java
+++ src/main/java/com/mysite/Ayoplanner/CommonUtil.java
@@ -1,5 +1,7 @@
 package com.mysite.Ayoplanner;
 
+import java.util.Random;
+
 import org.commonmark.node.Node;
 import org.commonmark.parser.Parser;
 import org.commonmark.renderer.html.HtmlRenderer;
@@ -7,10 +9,41 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class CommonUtil {
+
+	private Random rnd = new Random();
+
 	public String markdown(String markdown) {
 		Parser parser = Parser.builder().build();
 		Node document = parser.parse(markdown);
 		HtmlRenderer renderer = HtmlRenderer.builder().build();
 		return renderer.render(document);
+	}
+
+	public String createTempPassword() {
+		StringBuilder key = new StringBuilder();
+
+		for (int i = 0; i < 8; i++) { // 8자리
+			int index = rnd.nextInt(3);
+			switch (index) {
+			case 0:
+				key.append((char) ((rnd.nextInt(26)) + 97));
+				// a~z (ex. 1+97=98 => (char)98 = 'b')
+				break;
+			case 1:
+				key.append((char) ((rnd.nextInt(26)) + 65));
+				// A~Z
+				break;
+			case 2:
+				key.append((rnd.nextInt(10)));
+				// 0~9
+				break;
+			default:
+				key.append((char) ((rnd.nextInt(26)) + 97));
+				// a~z
+				break;
+			}
+		}
+
+		return key.toString();
 	}
 }
