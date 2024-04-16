0dbd39a1dadbd6d2dc6b1205c93a69690a765a37
Yunseo <158689708+yunseo531@users.noreply.github.com>
2024-04-16 16:56:54 +0900

    Update SecurityConfig.java

--- src/main/java/com/mysite/Ayoplanner/config/SecurityConfig.java
+++ src/main/java/com/mysite/Ayoplanner/config/SecurityConfig.java
@@ -15,58 +15,49 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-import com.mysite.Ayoplanner.snslogin.CustomOAuth2UserService;
+import com.mysite.Ayoplanner.sociallogin.CustomOAuth2UserService;
 
-    @Configuration
-    @EnableWebSecurity
-    @EnableMethodSecurity(prePostEnabled = true)
-    @RequiredArgsConstructor
-    public class SecurityConfig {
-        
-    	private final CustomOAuth2UserService customOAuth2UserService;
-        
-    	/**
-         * @param http
-         * @return
-         * @throws Exception
-         */
-    	
-        public static final String FRONT_URL = "http://localhost:8080";
-        
-        @Bean
-    	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        	http
-			.authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
-			.csrf(csrf -> csrf.disable())
-			.headers((headers) -> headers
-				.addHeaderWriter(new XFrameOptionsHeaderWriter(
-					XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN)))
-			.formLogin((formLogin) -> formLogin 
-					.loginPage("/login") 
-					.usernameParameter("email") 
-					.defaultSuccessUrl("/")) 
-			.logout((logout) -> logout
-				.logoutRequestMatcher(new AntPathRequestMatcher("/logout")) 
-				.logoutSuccessUrl("/")
-				.invalidateHttpSession(true))
-			.oauth2Login((oauth2Login) -> oauth2Login
-					.loginPage("/oauth2/authorization")
-					.defaultSuccessUrl("/")
-				.userInfoEndpoint(userInfoEndpointConfig -> userInfoEndpointConfig.userService(customOAuth2UserService))
-				)
-			;
-    		return http.build();
-        }
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
+@RequiredArgsConstructor
+public class SecurityConfig {
 
-		@Bean
-        PasswordEncoder passwordEncoder() {
-            return new BCryptPasswordEncoder();
-        }
-        
-        @Bean
-        AuthenticationManager authenticationManager(
-            AuthenticationConfiguration authenticationConfiguration)
-            throws Exception {
-            return authenticationConfiguration.getAuthenticationManager();
-        }
-    }
+	private final CustomOAuth2UserService customOAuth2UserService;
+
+	/**
+	 * @param http
+	 * @return
+	 * @throws Exception
+	 */
+
+	@Bean
+	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
+				.requestMatchers(new AntPathRequestMatcher("/**")).permitAll())
+	    	.authorizeHttpRequests((authorize) -> authorize
+	        .anyRequest().authenticated())
+				.csrf(csrf -> csrf.disable())
+				.headers((headers) -> headers
+						.addHeaderWriter(new XFrameOptionsHeaderWriter(
+							XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN)))
+				.formLogin(formLogin -> formLogin.loginPage("/login").usernameParameter("email").defaultSuccessUrl("/"))
+				.logout(logout -> logout.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+						.logoutSuccessUrl("/").invalidateHttpSession(true))
+				.oauth2Login(oauth2Login -> oauth2Login.loginPage("/login")
+						.defaultSuccessUrl("/")
+						.userInfoEndpoint(userInfoEndpoint -> userInfoEndpoint.userService(customOAuth2UserService)));
+		return http.build();
+	}
+
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+			throws Exception {
+		return authenticationConfiguration.getAuthenticationManager();
+	}
+}
