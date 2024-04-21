3d90de43f3dbb77807f267f43197ec08a47c6184
Yunseo <158689708+yunseo531@users.noreply.github.com>
2024-04-21 21:46:59 +0900

    Update TempPasswordMail.java

--- src/main/java/com/mysite/Ayoplanner/user/TempPasswordMail.java
+++ src/main/java/com/mysite/Ayoplanner/user/TempPasswordMail.java
@@ -16,49 +16,46 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class TempPasswordMail implements MailService {
 
-    private final JavaMailSender javaMailSender;
+	private final JavaMailSender javaMailSender;
 
-    private String ePw;
+	private String ePw;
 
-    @Override
-    public MimeMessage createMessage(String to)
-        throws UnsupportedEncodingException, MessagingException {
-        MimeMessage message = javaMailSender.createMimeMessage();
+	@Override
+	public MimeMessage createMessage(String to) throws UnsupportedEncodingException, MessagingException {
+		MimeMessage message = javaMailSender.createMimeMessage();
 
-        message.addRecipients(RecipientType.TO, to);
-        message.setSubject("Ayoplanner 임시 비밀번호");
+		message.addRecipients(RecipientType.TO, to);
+		message.setSubject("Ayoplanner 임시 비밀번호");
 
-        String msgg = "";
-        msgg += "<div style='margin:100px;'>";
-        msgg +=
-            "<div align='center' style='border:1px solid black; font-family:verdana';>";
-        msgg += "<h3 style='color:blue;'>임시 비밀번호입니다.</h3>";
-        msgg += "<div style='font-size:130%'>";
-        msgg += "CODE : <strong>";
-        msgg += ePw + "</strong><div><br/> ";
-        msgg += "</div>";
-        message.setText(msgg, "utf-8", "html");
-        message.setFrom(new InternetAddress("cbk425@gmail.com", "AyoPlanner"));
+		String msgg = "";
+		msgg += "<div style='margin:100px;'>";
+		msgg += "<div align='center' style='border:1px solid black; font-family:verdana';>";
+		msgg += "<h3 style='color:#89d8d3;'>임시 비밀번호</h3>";
+		msgg += "<div style='font-size:130%'>";
+		msgg += ePw + "</strong><div><br/> ";
+		msgg += "</div>";
+		message.setText(msgg, "utf-8", "html");
+		message.setFrom(new InternetAddress("cbk425@gmail.com", "AyoPlanner"));
 
-        return message;
-    }
+		return message;
+	}
 
-    @Override
-    public void sendSimpleMessage(String to, String pw) {
-        this.ePw = pw;
-        MimeMessage message;
-        try {
-            message = createMessage(to);
-        } catch (UnsupportedEncodingException | MessagingException e) {
-            e.printStackTrace();
-            throw new EmailException(ErrorCode.CREATE_MAIL_FAIL);
-        }
-        try {
-            javaMailSender.send(message);
-        } catch (MailException e) {
-            e.printStackTrace();
-            throw new EmailException(ErrorCode.SEND_MAIL_FAIL);
-        }
-    }
+	@Override
+	public void sendSimpleMessage(String to, String pw) {
+		this.ePw = pw;
+		MimeMessage message;
+		try {
+			message = createMessage(to);
+		} catch (UnsupportedEncodingException | MessagingException e) {
+			e.printStackTrace();
+			throw new EmailException(ErrorCode.CREATE_MAIL_FAIL);
+		}
+		try {
+			javaMailSender.send(message);
+		} catch (MailException e) {
+			e.printStackTrace();
+			throw new EmailException(ErrorCode.SEND_MAIL_FAIL);
+		}
+	}
 
 }
