c3829a29f2bc14bd9d17ccd3a8a7dbdb8730b1ce
Yunseo <158689708+yunseo531@users.noreply.github.com>
2024-04-27 17:00:07 +0900

    Delete makingplans directory

--- makingplans/SavePlanRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mysite.Ayoplanner.makingplans;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SavePlanRepository extends JpaRepository<SavePlan, Integer>{
-
-}
