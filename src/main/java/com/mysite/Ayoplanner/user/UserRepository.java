b74b9e1d26396e08a988637296302918e3b160b5
Yunseo <158689708+yunseo531@users.noreply.github.com>
2024-04-27 17:37:35 +0900

    Update UserRepository.java

--- src/main/java/com/mysite/Ayoplanner/user/UserRepository.java
+++ src/main/java/com/mysite/Ayoplanner/user/UserRepository.java
@@ -4,8 +4,6 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-//기본적인 CRUD 함수를 가지고 있음
-//JpaRepository를 상속했기 때문에 @Repository 어노테이션 불필요
 public interface UserRepository extends JpaRepository<SiteUser, Long> {// SiteUser의 기본키 타입은 Long
 	SiteUser findByid(Long id);
 
