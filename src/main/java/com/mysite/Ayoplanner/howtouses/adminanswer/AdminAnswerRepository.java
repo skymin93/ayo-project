9c3fa152f57fec8182a239ec103db88b1ba1b4ae
skymin93 <160587243+skymin93@users.noreply.github.com>
2024-04-27 17:56:08 +0900

    Add files via upload

---
+++ src/main/java/com/mysite/Ayoplanner/howtouses/adminanswer/AdminAnswerRepository.java
@@ -0,0 +1,7 @@
+package com.mysite.Ayoplanner.howtouses.adminanswer;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdminAnswerRepository extends JpaRepository<AdminAnswer, Integer>{
+
+}
