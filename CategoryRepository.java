938329c07cbc15baad096d12495761c22bf5c1a4
Yunseo <158689708+yunseo531@users.noreply.github.com>
2024-04-27 17:08:44 +0900

    Add files via upload

---
+++ CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.mysite.Ayoplanner.community.category;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
+
+	Optional<Category> findByName(String name);
+
+}
