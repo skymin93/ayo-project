9e9a70ed5522bd68cc400a6128a38b1550f3f47d
Yunseo <158689708+yunseo531@users.noreply.github.com>
2024-04-27 17:39:52 +0900

    Update and rename AyoplannerApplication.java to src/main/java/com/mysite/Ayoplanner/AyoplannerApplication.java

--- src/main/java/com/mysite/Ayoplanner/AyoplannerApplication.java
+++ src/main/java/com/mysite/Ayoplanner/AyoplannerApplication.java
@@ -3,8 +3,8 @@ package com.mysite.Ayoplanner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication //스프링부트 애플리케이션 시작 애너테이션
-public class AyoplannerApplication { //스프링부트애플리케이션 시작 담당 파일
+@SpringBootApplication
+public class AyoplannerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AyoplannerApplication.class, args);
