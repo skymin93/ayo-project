0952b09cb0394a4b56bfc8a4d2d704d994aed5a8
Yunseo <158689708+yunseo531@users.noreply.github.com>
2024-04-27 17:39:21 +0900

    Add files via upload

---
+++ AyoplannerApplication.java
@@ -0,0 +1,13 @@
+package com.mysite.Ayoplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication //스프링부트 애플리케이션 시작 애너테이션
+public class AyoplannerApplication { //스프링부트애플리케이션 시작 담당 파일
+
+	public static void main(String[] args) {
+		SpringApplication.run(AyoplannerApplication.class, args);
+	}
+
+}
