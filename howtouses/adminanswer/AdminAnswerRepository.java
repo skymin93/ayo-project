a9f3c5bcefa9c9396badc6ba141567519360c0e9
Yunseo <158689708+yunseo531@users.noreply.github.com>
2024-04-27 17:00:00 +0900

    Delete howtouses directory

--- howtouses/adminanswer/AdminAnswerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mysite.Ayoplanner.howtouses.adminanswer;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AdminAnswerRepository extends JpaRepository<AdminAnswer, Integer>{
-
-}
